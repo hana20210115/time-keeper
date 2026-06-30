0caa8cd1a9e2091a0665a745993986cc550fe10c
hana20210115 <hana20210115@gmail.com>
2026-06-30 13:43:30 +0900

    ログイン認証機能（一般ユーザー）のPHPUnitを用いたテストの実装。

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * バリデーションエラーの確認
+     */
+    public function test_ログインバリデーションエラーチェック(): void
+    {    
+        $user = User::factory()->create([
+            'email' =>'test7@example.com',
+            'password' =>bcrypt('password123'),
+        ]);
+        //メールアドレス未入力
+        $this->post('/login',[
+            'email'=>'',
+            'password'=>'password123',
+        ])->assertSessionHasErrors(['email'=>'メールアドレスを入力して下さい']);
+
+        //パスワード未入力
+        $this->post('/login',[
+            'email'=>'test7@example.com',
+            'password'=>'',
+        ])->assertSessionHasErrors(['password'=>'パスワードを入力して下さい']);
+
+        // 登録内容と一致しない
+        $this->post('/login',[
+            'email' => 'test7@example.com',
+            'password' => '12345678',
+        ])->assertSessionHasErrors([
+            'email' => 'ログイン情報が登録されていません'
+        ]);
+    }
+
+    /**
+     * 正常なログインの確認
+     */
+
+    public function test_正常なログインで勤怠画面に遷移する():void
+    {
+        $user = User::factory()->create([
+            'email'=>'test8@example.com',
+            'password'=>bcrypt('password123'),
+        ]);
+
+        $response = $this->post('/login',[
+            'email'=>'test8@example.com',
+            'password'=>'password123'
+        ]);
+
+        $response->assertRedirect('/attendance');
+        $this->assertAuthenticatedAs($user);
+    }
+
+
+}
